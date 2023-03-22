16fba72e4c585dc147fc73518d13c07e20a38767
Marcin <gasiorek.mail@gmail.com>
2023-03-22 11:48:13 +0100

    static nested class comparator, add options : sort by what...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import domain.Employee;
+import domain.EmployeeComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee(101,"Ralph",2015),
+                new Employee(102,"Carole",2021),
+                new Employee(103,"John",2013),
+                new Employee(104,"Jim",2018) ));
+
+//        var comparator =  new EmployeeComparator<>();
+//        employees.sort(comparator);
+
+        employees.sort(new Employee.EmployeeComparator<>("yearStarted").reversed());
+        for(Employee e : employees){
+            System.out.println(e);
+        }
+
     }
 }
--- src/domain/EmployeeComparator.java
+++ src/domain/EmployeeComparator.java
@@ -1,2 +1,21 @@
-package domain;public class EmployeeComparator {
+package domain;
+
+import java.util.Comparator;
+
+public class EmployeeComparator <T extends Employee> implements Comparator<Employee> {
+    private String sortType;
+
+    public EmployeeComparator() {
+        this("name");
+    }
+
+    public EmployeeComparator(String sortType) {
+        this.sortType = sortType;
+    }
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+
+        return o1.getName().compareTo(o2.getName());
+    }
 }
