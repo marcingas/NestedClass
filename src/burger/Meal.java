3626abf9f35583a6c8de2cf7462baba5e953f1e6
Marcin <gasiorek.mail@gmail.com>
2023-03-23 08:41:46 +0100

    inner class implementation for burger application constructor and run method.

--- src/burger/Meal.java
+++ src/burger/Meal.java
@@ -1,10 +1,23 @@
 package burger;
 
 public class Meal {
-    private double base = 5.0;
+    private double price = 5.0;
     private Item burger;
     private Item drink;
     private Item side;
+
+    public Meal() {
+        burger = new Item("regular", "burger");
+        drink = new Item("coke","drink", 1.5);
+        System.out.println(drink.name);
+        side = new Item("fries","side",2.0);
+    }
+
+    @Override
+    public String toString() {
+        return "%s%n%s%n%s%n".formatted(burger,drink,side);
+
+    }
 
     private class Item {
         private String name;
@@ -12,7 +25,7 @@ public class Meal {
         private double price;
 
         public Item(String name, String type) {
-            this(name, type, 0);
+            this(name, type, type.equals("burger") ? Meal.this.price : 0);
         }
 
         public Item(String name, String type, double price) {
---
+++ src/burger/Store.java
@@ -0,0 +1,8 @@
+package burger;
+
+public class Store {
+    public static void main(String[] args) {
+        Meal regularMeal = new Meal();
+        System.out.println(regularMeal);
+    }
+}
