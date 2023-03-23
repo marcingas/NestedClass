687824da08931df2be7566a4a73cab5ad8534fc1
Marcin <gasiorek.mail@gmail.com>
2023-03-23 09:28:47 +0100

    add toppings with inner class

--- src/burger/Meal.java
+++ src/burger/Meal.java
@@ -1,8 +1,11 @@
 package burger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
     private double price = 5.0;
-    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
     private double conversionRate;
@@ -13,13 +16,13 @@ public class Meal {
 
     public Meal(double conversionRate) {
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+        burger = new Burger("regular");
         drink = new Item("coke","drink", 1.5);
         System.out.println(drink.name);
         side = new Item("fries","side",2.0);
     }
     public double getTotal(){
-        double total = burger.price + drink.price + side.price;
+        double total = burger.getPrice() + drink.price + side.price;
         return Item.getPRice(total,conversionRate);
     }
 
@@ -28,11 +31,17 @@ public class Meal {
         return "%s%n%s%n%s%n%26s$%.2f".formatted(burger,drink,side,"Total due: ", getTotal());
 
     }
+    public void addToppings(String... selectedToppings){
+        burger.addToppings(selectedToppings);
+    }
 
     private class Item {
         private String name;
         private String type;
         private double price;
+        public Item(){
+
+        }
 
         public Item(String name, String type) {
             this(name, type, type.equals("burger") ? Meal.this.price : 0);
@@ -50,6 +59,52 @@ public class Meal {
         }
         private static double getPRice(double price, double rate){
             return price * rate;
+        }
+    }
+    private class Burger extends Item{
+        private enum Extra {AVOCADO,BACON,CHEESE,KETCHUP,MAYO,MUSTARD,PICKLES;
+        private double getPrice(){
+            return switch (this){
+                case AVOCADO -> 1.0;
+                case CHEESE, BACON -> 1.5;
+                default -> 0;
+            };
+        }
+        }
+
+        private List<Item>toppings = new ArrayList<>();
+
+
+
+         Burger(String name) {
+           super(name,"burger",5.0);
+        }
+        public double getPrice(){
+            double total = super.price;
+            for(Item topping : toppings){
+                total += topping.price;
+            }
+            return total;
+        }
+        private void addToppings(String... selectedToppings){
+             for(String selectedTopping : selectedToppings){
+                 try{
+                 Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+                 toppings.add(new Item(topping.name(), "TOPPING", topping.getPrice()));
+                 }catch (IllegalArgumentException e){
+                     System.out.println(selectedTopping + " Doesn't Exist");
+                 }
+             }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder itemized = new StringBuilder(super.toString());
+            for(Item topping : toppings){
+                itemized.append("\n");
+                itemized.append(topping);
+            }
+            return itemized.toString();
         }
     }
 }
--- src/burger/Store.java
+++ src/burger/Store.java
@@ -1,8 +1,11 @@
 package burger;
 
+
+
 public class Store {
     public static void main(String[] args) {
         Meal regularMeal = new Meal();
+        regularMeal.addToppings("Jupitero","Ketchup","Mayo","Bacon","JAMAOOO");
         System.out.println(regularMeal);
         Meal usMeal = new Meal(0.68);
         System.out.println(usMeal);
