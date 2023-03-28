c8ec7a888c57536e799dc9b5b853ae9991e3c91d
Marcin <gasiorek.mail@gmail.com>
2023-03-28 11:55:08 +0200

    create local class in new method addPigLatinName

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,61 @@ public class Main {
     public static void main(String[] args) {
 
         List<Employee> employees = new ArrayList<>(List.of(
-                new Employee(101,"Ralph",2015),
-                new Employee(102,"Carole",2021),
-                new Employee(103,"John",2013),
-                new Employee(104,"Jim",2018) ));
+                new Employee(101, "Ralph", 2015),
+                new Employee(102, "Carole", 2021),
+                new Employee(103, "John", 2013),
+                new Employee(104, "Jim", 2018)));
 
 //        var comparator =  new EmployeeComparator<>();
 //        employees.sort(comparator);
 
         employees.sort(new Employee.EmployeeComparator<>("yearStarted").reversed());
-        for(Employee e : employees){
+        for (Employee e : employees) {
             System.out.println(e);
         }
-        System.out.println("Store Members");
-        List<StoreEmployee>storeEmployees = new ArrayList<>(List.of(
-                new StoreEmployee(10015,"Meg",2014,"Target"),
-                new StoreEmployee(10016,"Joe",2017,"Walmart"),
-                new StoreEmployee(10017,"Tom",2019,"Macys"),
-                new StoreEmployee(10018,"Joy",2013,"Target"),
-                new StoreEmployee(10019,"Troy",2011,"Walmart")));
+        System.out.println("\nStore Members===========");
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg", 2014, "Target"),
+                new StoreEmployee(10016, "Joe", 2017, "Walmart"),
+                new StoreEmployee(10017, "Tom", 2019, "Macys"),
+                new StoreEmployee(10018, "Joy", 2013, "Target"),
+                new StoreEmployee(10019, "Troy", 2011, "Walmart")));
 
         var comparator = new StoreEmployee().new StoreComparator<>();
         storeEmployees.sort(comparator);
-        for(StoreEmployee e : storeEmployees){
+
+
+        for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
+        }
+        System.out.println("\n=====With pigLatinNames=====");
+        addPigLatinName(storeEmployees);
+
+    }
+
+    public static void addPigLatinName(List<? extends StoreEmployee> list) {
+        class DecoratedEmployee extends StoreEmployee {
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + " " + pigLatinName;
+            }
+        }
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size());
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+        for (var dEmployee : newList) {
+            System.out.println(dEmployee);
         }
     }
 }
